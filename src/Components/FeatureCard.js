6791b30617a05d852cb361ff0fdf488fd8eaed7b
Afahnyu Pila <AfahnyuPila03>
2023-11-29 23:59:10 +0100

    more modifications to bottom navigation component, added handleSelect function

--- src/Components/FeatureCard.js
+++ src/Components/FeatureCard.js
@@ -2,11 +2,12 @@ import {Text, TouchableOpacity} from 'react-native';
 import Card from './Card';
 
 export default function ({data, onSelect}) {
-  const {title, id} = data;
+  const {title, Icon} = data;
 
   return (
-    <TouchableOpacity>
-      <Text style={{color: "white", fontSize: 20}}>{title}</Text>
+    <TouchableOpacity onPress={onSelect}>
+      {/* <Icon /> */}
+      <Text style={{color: 'white', fontSize: 20}}>{title}</Text>
     </TouchableOpacity>
   );
 }
